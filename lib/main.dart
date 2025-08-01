520712edc00bd28be84186cd7dc59b03d47dc827
qaisarjamal01 <qaisarjamaluop@gmail.com>
2025-08-01 18:05:09 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:biometric_auth/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(),
+      home: const HomePage(),
     );
   }
 }
